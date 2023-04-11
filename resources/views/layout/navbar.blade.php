eed5e60390fbe1af87fbd99ccc8b3bc911d9b495
riskya3004 <riskyaputra3004@gmail.com>
2023-04-11 22:23:35 +0700

    update upload

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,12 +1,12 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Navbar</a>
+<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-edit" >
+    <a class="navbar-brand" href="/product">Webprog | Quiz</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <a class="nav-item nav-link active" href="#">Profile</a>
-        <a class="nav-item nav-link" href="/product">Products</a>
+        <a class="nav-item nav-link active" href="/product">Products</a>
+        <a class="nav-item nav-link" href="/penjelasan">Description</a>
       </div>
     </div>
   </nav>
